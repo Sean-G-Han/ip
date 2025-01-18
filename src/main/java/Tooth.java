e3b20bb8befa1e8162f30317eadcda6491f403b2
Sean-G-Han <156769273+Sean-G-Han@users.noreply.github.com>
2025-01-18 20:58:04 +0800

    Change Duke.java to Tooth.java

---
+++ src/main/java/Tooth.java
@@ -0,0 +1,12 @@
+public class Tooth {
+    public static void main(String[] args) {
+        String toothLogo =
+                        "  / \\      / \\    \n" +
+                        " |           |    \n" +
+                        " | |X|  |X|  |    \n" +
+                        " |    0      |    \n" +
+                        " \\ [][][][]  /    \n" +
+                        "  \\_________/     \n";
+        System.out.println(toothLogo + "Hello I am Tooth\n");
+    }
+}
