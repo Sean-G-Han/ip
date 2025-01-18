a52a04c655dee66f6fac1d233b21166296af9085
Sean-G-Han <156769273+Sean-G-Han@users.noreply.github.com>
2025-01-18 21:05:33 +0800

    Added Greetings

--- src/main/java/Tooth.java
+++ src/main/java/Tooth.java
@@ -7,6 +7,10 @@ public class Tooth {
                         " |    0      |    \n" +
                         " \\ [][][][]  /    \n" +
                         "  \\_________/     \n";
-        System.out.println(toothLogo + "Hello I am Tooth\n");
+        System.out.println("____________________________________________________________");
+        System.out.println(toothLogo + "Hello I am Tooth\n" + "What do you want?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye");
+        System.out.println("____________________________________________________________");
     }
 }
