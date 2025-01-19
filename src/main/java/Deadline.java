c2ea1b8b74b3ce6715f0fb12dd222be3f5fdfe96
Sean-G-Han <156769273+Sean-G-Han@users.noreply.github.com>
2025-01-19 14:43:43 +0800

    Small change to toString Method (added spacing)

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -21,6 +21,6 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(Due on: " + deadline + ")";
+        return "[D]" + super.toString() + " (Due on: " + deadline + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -22,6 +22,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(From: " + from + ", To: " + to + ")";
+        return "[E]" + super.toString() + " (From: " + from + ", To: " + to + ")";
     }
 }
