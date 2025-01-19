684a3b8fcf29e905586d2581987c865e3b2eef4f
Sean-G-Han <156769273+Sean-G-Han@users.noreply.github.com>
2025-01-19 18:51:40 +0800

    Added Testing for new Exceptions

--- src/main/java/Brain.java
+++ src/main/java/Brain.java
@@ -13,73 +13,110 @@ public class Brain {
         memory.add(item);
     }
 
-    public static void list() {
-        int i = 0;
-        for (Task task : memory) {
-            System.out.println(Integer.toString(i) + ": " + task);
-            i++;
+    public static void list() throws MemoryIsEmptyException {
+        if (memory.size() == 0) {
+            throw new MemoryIsEmptyException("Memory is Empty");
+        } else {
+            int i = 0;
+            for (Task task : memory) {
+                System.out.println(Integer.toString(i) + ": " + task);
+                i++;
+            }
         }
     }
 
-    public static void mark(int i) {
-        Task item = memory.get(i).complete();
-        memory.set(i, item);
-        System.out.println("Setting as marked: " + item);
+    public static void mark(int i) throws InvalidParamException{
+        if (i > memory.size()) {
+            throw new InvalidParamException("Index " + i + " is out of range");
+        } else {
+            Task item = memory.get(i).complete();
+            memory.set(i, item);
+            System.out.println("Setting as marked: " + item);
+        }
     }
 
-    public static void unmark(int i) {
-        Task item = memory.get(i).incomplete();
-        memory.set(i, item);
-        System.out.println("Setting as unmarked: " + item);
+    public static void unmark(int i) throws InvalidParamException {
+        if (i > memory.size()) {
+            throw new InvalidParamException("Index " + i + " is out of range");
+        } else {
+            Task item = memory.get(i).incomplete();
+            memory.set(i, item);
+            System.out.println("Setting as unmarked: " + item);
+        }
     }
 
-    public static void todo(String s) {
-        ToDo todo = ToDo.of(s);
-        add(todo);
+    public static void todo(String s) throws InvalidParamException {
+        if (s.isEmpty()) {
+            throw new InvalidParamException("Todo requires a description");
+        } else {
+            ToDo todo = ToDo.of(s);
+            add(todo);
+        }
     }
 
-    public static void event(String s) {
-        int fromIndex = s.indexOf("/from") + 6;
-        int toIndex = s.indexOf("/to") + 4;
-        String from = s.substring(fromIndex, toIndex - 4).trim();
-        String to = s.substring(toIndex).trim();
-        String item = s.substring(0, fromIndex - 6).trim();
-        Event event = Event.of(item, from, to);
-        add(event);
+    public static void event(String s) throws InvalidParamException {
+        int fromIndex = s.indexOf("/from");
+        int toIndex = s.indexOf("/to");
+        if (s.isEmpty()) {
+            throw new InvalidParamException("Event requires a description");
+        } else if (fromIndex == -1 || toIndex == -1) {
+            throw new InvalidParamException("Missing either /to or /from field");
+        } else if (fromIndex == 0) {
+            throw new InvalidParamException("Missing the title");
+        } else {
+            String from = s.substring(fromIndex + 6, toIndex).trim();
+            String to = s.substring(toIndex + 4).trim();
+            String item = s.substring(0, fromIndex).trim();
+            Event event = Event.of(item, from, to);
+            add(event);
+        }
     }
 
     public static void deadline(String s) {
-        int byIndex = s.indexOf("/by") + 4;
-        String by = s.substring(byIndex).trim();
-        String item = s.substring(0, byIndex - 4).trim();
-        Deadline deadline = Deadline.of(item, by);
-        add(deadline);
+        int byIndex = s.indexOf("/by");
+        if (s.isEmpty()) {
+            throw new InvalidParamException("Event requires a description");
+        } else if (byIndex == -1) {
+            throw new InvalidParamException("Missing /by field");
+        } else if (byIndex == 0) {
+            throw new InvalidParamException("Missing the title");
+        } else {
+            String by = s.substring(byIndex + 4).trim();
+            String item = s.substring(0, byIndex).trim();
+            Deadline deadline = Deadline.of(item, by);
+            add(deadline);
+        }
     }
 
     public static void processing() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            String input = scanner.nextLine();
-            String[] command = input.split(" ");
-            String s = String.join(" ", Arrays.copyOfRange(command, 1, command.length));
-            if (command[0].equals("bye"))
-                break;
-            else if (command[0].equals("list"))
-                list();
-            else if (command[0].equals("mark")) {
-                int i = Integer.parseInt(command[1]);
-                mark(i);
+            try {
+                String input = scanner.nextLine();
+                String[] command = input.split(" ");
+                String s = String.join(" ", Arrays.copyOfRange(command, 1, command.length));
+                if (command[0].equals("bye"))
+                    break;
+                else if (command[0].equals("list"))
+                    list();
+                else if (command[0].equals("mark")) {
+                    int i = Integer.parseInt(command[1]);
+                    mark(i);
+                } else if (command[0].equals("unmark")) {
+                    int i = Integer.parseInt(command[1]);
+                    unmark(i);
+                } else if (command[0].equals("todo"))
+                    todo(s);
+                else if (command[0].equals("event"))
+                    event(s);
+                else if (command[0].equals("deadline"))
+                    deadline(s);
+                else
+                    throw new InvalidCommandException("Command " + command[0] + " does not exist");
+
+            } catch (ToothException e) {
+                System.out.println(e.getMessage());
             }
-            else if (command[0].equals("unmark")) {
-                int i = Integer.parseInt(command[1]);
-                unmark(i);
-            }
-            else if (command[0].equals("todo"))
-                todo(s);
-            else if (command[0].equals("event"))
-                event(s);
-            else if (command[0].equals("deadline"))
-                deadline(s);
         }
     }
 }
