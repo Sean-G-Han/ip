15c43a0832e0e3ac3023245b1f672ca637c6c25f
Sean-G-Han <156769273+Sean-G-Han@users.noreply.github.com>
2025-02-01 16:48:28 +0800

    Init JUnit

---
+++ src/test/java/Tooth.java
@@ -0,0 +1,32 @@
+import tooth.command.ByeCommand;
+import tooth.command.Command;
+import tooth.exception.ToothException;
+import tooth.stuff.Parser;
+import tooth.stuff.Storage;
+import tooth.stuff.TaskList;
+import tooth.stuff.UI;
+
+import java.util.Scanner;
+
+public class Tooth {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Storage storage = new Storage();
+        TaskList tasks = new TaskList();
+        UI ui = new UI();
+        Parser parser = new Parser();
+        ui.welcome();
+        while (true) {
+            try {
+                String input = scanner.nextLine();
+                Command c = parser.parse(input);
+                c.execute(tasks, ui, storage);
+                if (c instanceof ByeCommand)
+                    break;
+            } catch (ToothException e) {
+                ui.complain(e.getMessage());
+            }
+        }
+    }
+}
---
+++ src/test/java/tooth/stuff/ParserTest.java
@@ -0,0 +1,18 @@
+package tooth.stuff;
+
+import org.junit.jupiter.api.Test;
+import tooth.command.Command;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParserTest {
+    @Test
+    public void dummyTest(){
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void anotherDummyTest(){
+        assertEquals(4, 4);
+    }
+}
